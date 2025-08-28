f22d6ba835a2b6b11f8093a0cc8ecc9722dcda32
pratik1729 <gaikwadpratik1729@gmail.com>
2025-08-28 20:05:41 +0530

    Experiment tracking usign dvclive done

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,6 +5,8 @@ import pickle
 import json
 import logging
 from sklearn.metrics import accuracy_score,precision_score,recall_score,roc_auc_score
+import yaml
+from dvclive import Live
 
 log_dir = 'logs'
 os.makedirs(log_dir,exist_ok=True)
@@ -25,6 +27,22 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(param_path:str)->dict:
+    try:
+        with open(param_path,'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Prams are retrieved from the %s',param_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found %s',param_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('Yaml error %s',e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error %s',e)
+        raise
 
 def load_model(file_path:str):
     """Load the trained model from a file"""
@@ -90,6 +108,7 @@ def save_metrics(metrics:dict,file_path:str)->None:
 
 def main():
     try:
+        params = load_params(param_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -97,6 +116,12 @@ def main():
         y_test = test_data.iloc[:,-1].values
 
         metrics = evaluate_model(clf,X_test,y_test)
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy',accuracy_score(y_test,y_test))
+            live.log_metric('precision' , precision_score(y_test,y_test))
+            live.log_metric('recall',recall_score(y_test,y_test))
+
+            live.log_params(params)
 
         save_metrics(metrics,'reports/metrics.json')
     except Exception as e:
